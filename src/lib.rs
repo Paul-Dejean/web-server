08809fd577678552135c912776514437dab2445c
Ebooth <pauldejeandev@gmail.com>
2025-02-22 17:18:47 +0100

    [Step2] Modified the server to serve html documents

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use std::fs;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
+
+const STATIC_FILES_DIR_PATH: &str = "www";
 
 pub fn launch_server(addr: &str) -> std::io::Result<()> {
     let listener = TcpListener::bind(addr)?;
@@ -19,23 +22,119 @@ pub fn launch_server(addr: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut reader = BufReader::new(&stream);
+#[derive(Debug)]
+enum HttpError {
+    BadRequest(String),
+    NotFound(String),
+    InternalServerError(String),
+}
 
-    let mut line = String::new();
-    reader.read_line(&mut line)?;
+impl From<std::io::Error> for HttpError {
+    fn from(e: std::io::Error) -> Self {
+        HttpError::BadRequest(e.to_string())
+    }
+}
+fn parse_path(path: &str) -> Result<String, &'static str> {
+    if path.contains("..") {
+        return Err("Invalid path");
+    }
 
-    let parts = line.split_whitespace().collect::<Vec<_>>();
-    if let [_, path, _] = parts.as_slice() {
-        let response = format!("HTTP/1.1 200 OK\r\n\r\nRequested path: {}\r\n", path);
-        stream.write_all(response.as_bytes())?;
-        stream.flush()?;
+    if path == "/" {
+        return Ok(format!("{}/index.html", STATIC_FILES_DIR_PATH));
+    }
 
-        Ok(())
-    } else {
+    Ok(format!("{}/{}", STATIC_FILES_DIR_PATH, &path[1..]))
+}
+
+fn send_error_response(stream: &mut TcpStream, error: &HttpError) -> std::io::Result<()> {
+    let (status_line, message) = match error {
+        HttpError::BadRequest(msg) => ("HTTP/1.1 400 Bad Request", msg),
+        HttpError::NotFound(msg) => ("HTTP/1.1 404 Not Found", msg),
+        HttpError::InternalServerError(msg) => ("HTTP/1.1 500 Internal Server Error", msg),
+    };
+
+    let response = format!("{}\r\n\r\n{}", status_line, message);
+
+    if let Err(e) = stream.write_all(response.as_bytes()) {
+        eprintln!("Failed to write error response: {}", e);
+        return Err(e);
+    }
+
+    if let Err(e) = stream.flush() {
+        eprintln!("Failed to flush error response: {}", e);
         return Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidData,
-            "Invalid request",
+            std::io::ErrorKind::Other,
+            "Internal Server Error (flush failed)",
         ));
     }
+
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    let result = process_request(&mut stream);
+
+    if let Err(err) = result {
+        // Attempt to send the error response. We ignore errors from send_error_response here,
+        // but you might choose to log them.
+        let _ = send_error_response(&mut stream, &err);
+        return match err {
+            HttpError::NotFound(msg) => Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                format!("Not Found: {}", msg),
+            )),
+            HttpError::BadRequest(msg) => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Bad Request: {}", msg),
+            )),
+            HttpError::InternalServerError(msg) => Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Internal Server Error: {}", msg),
+            )),
+        };
+    }
+    Ok(())
+}
+
+fn parse_header(stream: &mut TcpStream) -> Result<[String; 3], HttpError> {
+    let mut reader = BufReader::new(stream);
+    let mut line = String::new();
+    reader
+        .read_line(&mut line)
+        .map_err(|e| HttpError::BadRequest(e.to_string()))?;
+    let parts: Vec<&str> = line.split_whitespace().collect();
+    match parts.as_slice() {
+        [method, path, protocol] => {
+            return Ok([method.to_string(), path.to_string(), protocol.to_string()])
+        }
+        _ => return Err(HttpError::BadRequest("Invalid request".into())),
+    }
+}
+
+fn process_request(stream: &mut TcpStream) -> Result<(), HttpError> {
+    let [_, path, _] = parse_header(stream)?;
+    let file_path = parse_path(&path).map_err(|e| HttpError::BadRequest(e.to_string()))?;
+
+    let contents = fs::read(file_path).map_err(|e| {
+        if e.kind() == std::io::ErrorKind::NotFound {
+            HttpError::NotFound("Page Not Found".into())
+        } else {
+            HttpError::InternalServerError(e.to_string())
+        }
+    })?;
+
+    let response_header = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream
+        .write_all(response_header.as_bytes())
+        .map_err(|e| HttpError::BadRequest(e.to_string()))?;
+    stream
+        .write_all(&contents)
+        .map_err(|e| HttpError::BadRequest(e.to_string()))?;
+
+    stream
+        .flush()
+        .map_err(|e| HttpError::InternalServerError(e.to_string()))?;
+
+    Ok(())
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,9 +2,9 @@ use std::{thread, time::Duration};
 use web_server::launch_server;
 
 #[test]
-fn should_respond_with_requested_path() {
+fn should_respond_with_requested_file() {
     let addr = "127.0.0.1:8000";
-    let path = "/hello";
+    let path = "/index.html";
     thread::spawn(move || {
         launch_server(addr).unwrap();
     });
@@ -12,9 +12,54 @@ fn should_respond_with_requested_path() {
     let response = reqwest::blocking::get(format!("http://{}{}", addr, path))
         .expect("Failed to connect to server");
     let body = response.text().expect("Failed to read response body");
+
+    let expected_content = std::fs::read_to_string("www/index.html").expect("Failed to read file");
+
     assert!(
-        body.contains(format!("Requested path: {}", path).as_str()),
+        body.contains(&expected_content),
         "Unexpected body: {}",
+        body
+    );
+}
+
+#[test]
+fn should_respond_with_default_file() {
+    let addr = "127.0.0.1:8000";
+    let path = "/";
+    thread::spawn(move || {
+        launch_server(addr).unwrap();
+    });
+    thread::sleep(Duration::from_millis(200));
+    let response = reqwest::blocking::get(format!("http://{}{}", addr, path))
+        .expect("Failed to connect to server");
+    let body = response.text().expect("Failed to read response body");
+
+    let expected_content = std::fs::read_to_string("www/index.html").expect("Failed to read file");
+
+    assert!(
+        body.contains(&expected_content),
+        "Unexpected body: {}",
+        body
+    );
+}
+
+#[test]
+fn should_respond_with_not_found() {
+    let addr = "127.0.0.1:8000"; // Use a different port if needed
+    let path = "/nonexistent.html";
+    thread::spawn(move || {
+        launch_server(addr).unwrap();
+    });
+    thread::sleep(Duration::from_millis(200));
+    let response = reqwest::blocking::get(format!("http://{}{}", addr, path))
+        .expect("Failed to connect to server");
+
+    assert_eq!(response.status(), reqwest::StatusCode::NOT_FOUND);
+    let body = response.text().expect("Failed to read response body");
+
+    assert!(
+        body.contains("Page Not Found"),
+        "Unexpected body for error: {}",
         body
     );
 }
